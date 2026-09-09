38749b592a2ea347d1a9a212a0b0d9ec238d130e
WalterCastroSena <cipagautaesteban@gmail.com>
2026-09-09 01:27:08 +0000

    Cambios

--- buscadorPokemon/src/app/components/registro-usuario/registro-usuario.component.ts
+++ buscadorPokemon/src/app/components/registro-usuario/registro-usuario.component.ts
@@ -1,80 +1,61 @@
-import { ThisReceiver } from '@angular/compiler';
 import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms'
-
-//Primera tarea definir el modelo de datos
+import { FormsModule } from '@angular/forms';
 
 export interface Usuario {
-  id: number;
-  nombreCompleto: string; 
-  documento: {
-  tipo: string;
-  num: number;
-        };
-  correo: string;
-  celu: number;
-  fecha_nac: Date;
-  ubicacion: { 
-  pais: string;   
-  ciudad: string;
-        };
-  tratamientos: boolean;
-};
-//hasta aqui el modelado
+    id : number;
+    nombreCompleto : string;
+    documento : { 
+      tipo: string;
+      numero: string
+    };
+    fechaNacimiento : string;
+    correo : string;
+    datosPersonales : boolean;  
+    fechaRegistro: string;
 
-// Sigue el component, me ayuda a tener toda la log
+}
+
 
 @Component({
-  selector: 'app-registro-usuario',
-  standalone: true, //Declara que un componente es autonomo, esto por lo general es una buena parcticar colocar un componente sea autonomo
   imports: [FormsModule],
+  selector: 'app-registro-usuario',
+  standalone: true,
+  styleUrl: './registro-usuario.component.css',
   templateUrl: './registro-usuario.component.html',
-  styleUrl: './registro-usuario.component.css'
 })
 export class RegistroUsuarioComponent {
-
-  //Vamos a aplicarle una inicializacon con signal
-
   nombre = signal('');
   apellido = signal('');
-  identificacion = signal('CC');
-  numero = signal('');
-  numero_identificacion = signal('');
-  celu = signal('');
+  tipo_doc = signal('CC');
+  dni = signal('');
+  fecha_nacimiento = signal('');
   correo = signal('');
-  fecha_nacimiento= signal('');
-  correo = signal('');
-  pais = signal('');
-  ciudad = signal('');
-  datos = signal('False');
+  datos_personales = signal(false);
 
-  ultimoUsuario = signal<Usuario | null>(null);
+    ultimoUsuario = signal<Usuario | null>(null);
 
-  guardarUsuario() {
-    if (this.datos()){
-      alert('Debes aceptar el tratamiento de datos personales');
-      return;
+    guardarUsuario() {
+        if(!this.datos_personales()){
+          console.error('Debes aceptar el tratamiento de datos personales');
+          return;
+        }
+
+        const usuarioCreado = {
+        id : Date.now(),
+        nombreCompleto : `${this.nombre()} ${this.apellido()}`,
+        documento : {
+          tipo: this.tipo_doc(),
+          numero: this.dni()
+        },
+        fechaNacimiento : this.fecha_nacimiento(),
+        correo : this.correo(),
+        datosPersonales : this.datos_personales(),  
+        fechaRegistro: new Date().toLocaleDateString()
     }
 
-    const usuarioCreado = {
-        id: Date.now(),
-        nombreCompleto: `${this.nombre()}, ${this.apellido()}`, 
-        documento: {
-            tipo: this.identificacion(),
-            num: this.numero_identificacion()
-        },
-        celular: this.celu(),
-        correo: this.correo(),
-        fecha_nac: this.fecha_nacimiento(),
-        ubicacion: { 
-            pais: this.pais(),     
-            ciudad: this.ciudad()
-        },
-        tratamientos: this.datos(),
-  }
+    localStorage.setItem(usuarioCreado.id.toString(), JSON.stringify(usuarioCreado));
 
-  localStorage.setItem(usuarioCreado.id.toString(),JSON.stringify(usuarioCreado));  
+    this.ultimoUsuario.set(usuarioCreado);
 
-  this.ultimoUsuario.set(usuarioCreado); 
-  }   
+    }
 }
